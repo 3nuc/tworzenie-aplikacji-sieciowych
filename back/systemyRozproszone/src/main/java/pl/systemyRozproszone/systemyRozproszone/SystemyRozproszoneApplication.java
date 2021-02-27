c1f4baad82a7f499bbf24bb743e3f1b5802a97bd
tonio965 <tonio96@o2.pl>
2021-02-27 19:38:27 +0100

    empty spring project with basic configuration

---
+++ back/systemyRozproszone/src/main/java/pl/systemyRozproszone/systemyRozproszone/SystemyRozproszoneApplication.java
@@ -0,0 +1,13 @@
+package pl.systemyRozproszone.systemyRozproszone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemyRozproszoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemyRozproszoneApplication.class, args);
+	}
+
+}
