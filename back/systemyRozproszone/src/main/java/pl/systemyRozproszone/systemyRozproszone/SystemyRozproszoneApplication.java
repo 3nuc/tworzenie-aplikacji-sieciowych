04c23313ce25be825b1e7b79859001556bba932f
tonio965 <tonio96@o2.pl>
2021-02-27 20:41:47 +0100

    dockerfile for java project

--- back/systemyRozproszone/src/main/java/pl/systemyRozproszone/systemyRozproszone/SystemyRozproszoneApplication.java
+++ back/systemyRozproszone/src/main/java/pl/systemyRozproszone/systemyRozproszone/SystemyRozproszoneApplication.java
@@ -2,8 +2,10 @@ package pl.systemyRozproszone.systemyRozproszone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SystemyRozproszoneApplication {
 
 	public static void main(String[] args) {
