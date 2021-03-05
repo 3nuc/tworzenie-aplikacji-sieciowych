cf5dcd7eef19764b2c4fa3e562bf585cdbc563e1
Artur Tagisow <artur.tagisow@billennium.com>
2021-03-05 16:10:24 +0100

    Add element-plus

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,9 +1,11 @@
 import 'windi.css'
 import './styles/main.css'
+import 'element-plus/lib/theme-chalk/index.css'
 import { ViteSSG } from 'vite-ssg'
 import generatedRoutes from 'pages-generated'
 import { setupLayouts } from 'layouts-generated'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
 
 const routes = setupLayouts(generatedRoutes)
 
@@ -14,5 +16,6 @@ export const createApp = ViteSSG(
   (ctx) => {
     // install all modules under `modules/`
     Object.values(import.meta.globEager('./modules/*.ts')).map(i => i.install?.(ctx))
+    ctx.app.use(ElementPlus)
   },
 )
