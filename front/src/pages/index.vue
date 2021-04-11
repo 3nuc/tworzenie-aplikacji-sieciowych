4515721b952265fed61b595d5d1d805532695785
Artur Tagisow <artur.tagisow@billennium.com>
2021-04-11 14:33:45 +0200

    remove apexcharts for real and fix icons build errors

--- front/src/pages/index.vue
+++ front/src/pages/index.vue
@@ -17,7 +17,7 @@ const { t } = useI18n()
 <template>
   <div>
     <p class="text-4xl">
-      <carbon-campsite class="inline-block" />
+      <vite-icon-carbon-campsite class="inline-block" />
     </p>
     <p>
       <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
--- front/src/pages/upload.vue
+++ front/src/pages/upload.vue
@@ -1,23 +1,22 @@
 <script setup lang="ts">
 const options = {
   chart: {
-    id: 'vuechart-example'
+    id: 'vuechart-example',
   },
   xaxis: {
-    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
-  }
-};
+    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+  },
+}
 const series = [{
   name: 'series-1',
-  data: [30, 40, 45, 50, 49, 60, 70, 91]
-}];
+  data: [30, 40, 45, 50, 49, 60, 70, 91],
+}]
 </script>
 
 <template>
   <el-upload drag>
-     Przeciągnij plik
+    Przeciągnij plik
   </el-upload>
-  <apexchart width="500" type="bar" v-bind="{options, series}"></apexchart>
 </template>
 
 <route lang="yaml">
