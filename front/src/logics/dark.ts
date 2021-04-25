6c2367ea79d078394dd5614e3b760c6822a8b53e
Artur Tagisow <artur.tagisow@billennium.com>
2021-04-25 11:45:03 +0200

    perma disable dark mode as elementplus doesnt support it

--- front/src/logics/dark.ts
+++ front/src/logics/dark.ts
@@ -6,7 +6,8 @@ const preferredDark = usePreferredDark()
 
 export const isDark = computed({
   get() {
-    return colorSchema.value === 'auto' ? preferredDark.value : colorSchema.value === 'dark'
+    //element plus doesn't support dark mode
+    return false
   },
   set(v: boolean) {
     if (v === preferredDark.value)
