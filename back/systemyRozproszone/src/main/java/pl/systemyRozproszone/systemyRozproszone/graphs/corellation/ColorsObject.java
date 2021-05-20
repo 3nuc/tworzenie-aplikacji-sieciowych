eeae43242146430b708d7bda6685fe18b40720ed
tonio965 <tonio96@o2.pl>
2021-05-20 22:38:41 +0200

    wysrywa tabelke lgbt pod jednym endpointem

---
+++ back/systemyRozproszone/src/main/java/pl/systemyRozproszone/systemyRozproszone/graphs/corellation/ColorsObject.java
@@ -0,0 +1,32 @@
+package pl.systemyRozproszone.systemyRozproszone.graphs.corellation;
+
+public class ColorsObject {
+
+    int h;
+    int s;
+    int l;
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
+    public int getS() {
+        return s;
+    }
+
+    public void setS(int s) {
+        this.s = s;
+    }
+
+    public int getL() {
+        return l;
+    }
+
+    public void setL(int l) {
+        this.l = l;
+    }
+}
