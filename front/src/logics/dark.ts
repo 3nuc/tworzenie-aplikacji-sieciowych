5bb464a2cb30cb38c0e4d2b1e16376761b06ec1e
Artur Tagisow <artur.tagisow@billennium.com>
2021-05-24 11:46:08 +0200

    fix dark mode

--- front/src/logics/dark.ts
+++ front/src/logics/dark.ts
@@ -1,19 +1,14 @@
 import { watch, computed } from 'vue'
-import { usePreferredDark, useToggle } from '@vueuse/core'
+import { useToggle } from '@vueuse/core'
 import { colorSchema } from './store'
-
-const preferredDark = usePreferredDark()
 
 export const isDark = computed({
   get() {
     //element plus doesn't support dark mode
     return false
   },
-  set(v: boolean) {
-    if (v === preferredDark.value)
-      colorSchema.value = 'auto'
-    else
-      colorSchema.value = v ? 'dark' : 'light'
+  set() {
+    colorSchema.value = 'light'
   },
 })
 
