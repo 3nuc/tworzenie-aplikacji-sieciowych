d681b4c21a02fcf19d4894800bfb99aeee03282f
tonio965 <tonio96@o2.pl>
2021-06-03 12:11:01 +0200

    chyba corsy sfixowane

---
+++ back/systemyRozproszone/src/main/java/pl/systemyRozproszone/systemyRozproszone/configuration/SimpleCORSFilter.java
@@ -0,0 +1,42 @@
+package pl.systemyRozproszone.systemyRozproszone.configuration;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Component
+public class SimpleCORSFilter implements Filter {
+
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        System.out.println("cors filter");
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        System.out.println("cors init");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("cors destroy");
+    }
+
+}
